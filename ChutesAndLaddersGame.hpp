e96a31a259d7105fa1d2a2e5ae8f1b9bfc942b9b
jcastillo2583 <bluejccastle@gmail.com>
2016-11-11 16:54:37 -0800

    Update ChutesAndLaddersGame.hpp

--- ChutesAndLaddersGame.hpp
+++ ChutesAndLaddersGame.hpp
@@ -21,8 +21,6 @@ public:
    // TODO: implement the constructor with all your team members
    // constructor with the default value of 2 minimim players
    ChutesAndLaddersGame(int nPlayers = MIN_NUMBER_OF_PLAYERS){
-    one = new Player("Anthony");
-    two = new Player("Justin");
    list.enqueue(one);
    list.enqueue(two);
    
@@ -64,8 +62,8 @@ public:
 //}
 private:
    const int WINNING_POSITION = 100;
-   Player one;
-   Player two;
+   Player one("Anthony");
+   Player two("Justin");
    string winner;       // the winner
    GameBoard gameBoard; // the game board
    ArrayQueue<Player> list;
